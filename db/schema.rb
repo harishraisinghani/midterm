8fe36f3a500eea8548cd28b55f0ba036923791a0
Kevin Teh <kfcteh@gmail.com>
2016-08-11 16:54:06 +0000

    completeted initial migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160811062915) do
+
+  create_table "favourites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "favourite_user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "rating"
+    t.integer  "user_type"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "userskills", force: :cascade do |t|
+    t.integer  "skill_id"
+    t.integer  "user_id"
+    t.integer  "years_experience"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
