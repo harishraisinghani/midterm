a34183581f6bd946dc4d542330327b0f5aa0b5a3
James Kirkpatrick <jkirkpat16@gmail.com>
2016-08-12 20:48:20 +0000

    implemented routing and search functions

--- app/actions.rb
+++ app/actions.rb
@@ -1,15 +1,37 @@
-helpers
+helpers do
+  def is_logged_in?
+    session[:user_id] != nil
+  end
+end
 
 # Homepage (Root path)
 get '/' do
+  @user = User.find(2)
+  session[:user_id] = @user.id
   erb :index
 end
 
+post '/' do
+  skill = Skill.find_by(name: params[:content])
+  skill_id = skill.id
+  if is_logged_in?
+    redirect "search/#{skill_id}"
+  end
+end
+
 get '/search/:skill_id' do
+  if is_logged_in?
+    @user = User.find(session[:user_id])
+  end
   erb :search
 end
 
 get '/profile/:id' do
+  if is_logged_in?
+    @user = User.find(session[:user_id])
+  else
+    redirect '/'
+  end
   erb :profile
 end
 
