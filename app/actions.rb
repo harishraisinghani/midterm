a633924fb89ea9407fb91ab8e4f0541be537dc2a
James Kirkpatrick <jkirkpat16@gmail.com>
2016-08-12 04:09:06 +0000

    added routing

--- app/actions.rb
+++ app/actions.rb
@@ -1,13 +1,34 @@
+helpers
+
 # Homepage (Root path)
 get '/' do
   erb :index
 end
 
-get '/search' do
+get '/search/:skill_id' do
   erb :search
 end
 
-get '/profile' do
+get '/profile/:id' do
   erb :profile
 end
 
+post '/favourites' do
+  "figure out yo faves"
+end
+
+post '/profile/feedbacks' do
+
+end
+
+delete '/profile/feedback/:id' do
+ "for feedback deletion"
+end
+
+post 'user/skills' do
+
+end
+
+post '/favouriteds' do
+
+end
